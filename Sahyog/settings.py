697b65243e836c31e9eee5f2b161e1dd07c13b92
niharikadhapola <47639810+niharikadhapola@users.noreply.github.com>
2023-07-28 22:18:18 +0530

    Update settings.py

--- Sahyog/settings.py
+++ Sahyog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'u4nob5u2@&34sw&e+(xkjpmngt9lnb-gy5cehg0q4v^09p&#ih'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
